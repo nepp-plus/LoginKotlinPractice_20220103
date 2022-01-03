ebaf0845dc0efb75da6adbc495d16f570b8e9c88
nepp-plus <nepp.plus@gmail.com>
2022-01-03 17:04:01 +0900

    코틀린의 비교연산 / 논리연산 / 조건문 체험

--- app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.loginkotlinpractice_20220103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +19,17 @@ class MainActivity : AppCompatActivity() {
             val inputId = edtEmail.text.toString()
             val inputPw = edtPassword.text.toString()
 
-//        2. id : admin / pw : qwer1234 두개가 모두 맞다면, 토스트로 "관리자입니다."
+//        2. id : admin (비교연산) / pw : qwer1234 두개가 모두 맞다면 (논리연산) + (조건문), 토스트로 "관리자입니다."
 //          => 둘중 하나라도 틀리다면 "로그인 실패." 토스트.
+
+            if (inputId == "admin" && inputPw == "qwer1234") {
+
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
 
         }
 
