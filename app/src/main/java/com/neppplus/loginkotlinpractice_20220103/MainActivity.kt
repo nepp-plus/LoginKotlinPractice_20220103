5dc3a6d6d19da2c79cffa7c4e09db8a0363a0844
nepp-plus <nepp.plus@gmail.com>
2022-01-03 16:55:43 +0900

    코틀린 - 작업 할 내용 주석

--- app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/loginkotlinpractice_20220103/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        로그인 버튼이 눌리면 할 일
+
+//        1. 입력한 아이디와 비번이 뭔지 변수에 담아두자.
+
+//        2. id : admin / pw : qwer1234 두개가 모두 맞다면, 토스트로 "관리자입니다."
+//          => 둘중 하나라도 틀리다면 "로그인 실패." 토스트.
+
     }
 }
